484ebfb1241661805ec3233f5398fc8f8ee6a177
Morteza Shahriari Nia <m.shahriarinia@gmail.com>
2015-01-19 22:54:35 -0500

    commit

---
+++ wordvector-project/src/main/java/edu/ufl/cise/wordvector/App.java
@@ -0,0 +1,13 @@
+package edu.ufl.cise.wordvector;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
